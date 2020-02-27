5110f69c5b7cf00362ba21d13a91be08c74fb209
pengcan985632236 <18810381615@163.com>
2020-02-27 13:50:12 +0800

    把工作目录中的文件添加到本地仓库

---
+++ springboot_websocket/src/main/java/com/example/springboot_websocket/SpringbootWebsocketApplication.java
@@ -0,0 +1,19 @@
+package com.example.springboot_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringbootWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebsocketApplication.class, args);
+    }
+
+
+
+
+
+}
